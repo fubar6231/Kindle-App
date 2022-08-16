51886b400c002180d93de01864d468192cecd733
Ashwani kumar <ashwani.singh.20.1@mountblue.tech>
2022-08-17 00:24:40 +0530

    fixed bug

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 
 const db = require('./src/config/db.config')
 const BooksRouter = require("./src/routes/BooksRoutes")
 
+
+
+
+app.use(cors())
 app.use(express.json())
 db.BooksLibrary = require('./src/models/Books')
 app.listen(process.env.PORT)
