a87ffd9f7259c00d231ecfee3c9cf98b86eebd45
Ashwani kumar <ashwani.singh.20.1@mountblue.tech>
2022-08-18 10:48:47 +0530

    added margin

--- Frontend/booktracking-app/src/Components/BookCard.js
+++ Frontend/booktracking-app/src/Components/BookCard.js
@@ -4,7 +4,7 @@ import {Button, Card, ListGroup} from "react-bootstrap";
 class BookCard extends Component {
     render() {
         return (
-            <Card style={{ width: '18rem' }}>
+            <Card className="m-2" style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F24%2F2018%2F09%2Fgettyimages-183822187-1.jpg" />
                 <Card.Body>
                     <Card.Title>{this.props.BookDetails.Title}</Card.Title>
